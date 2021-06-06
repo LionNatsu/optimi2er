abfb505c612985e6a96501a7aa0141297587da61
Daming Yang <lion@aosc.io>
2021-06-07 01:33:43 +0800

    codegen: generate an integer
    == Codegen ==
    i64 123

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <llvm/IR/Value.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/LLVMContext.h>
+
+llvm::Value *codegen(llvm::LLVMContext &ctx) {
+    auto t_int64 = llvm::IntegerType::get(ctx, 64);
+    return llvm::ConstantInt::get(t_int64, 123);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    llvm::LLVMContext ctx;
+    std::cout << "== Codegen ==" << std::endl;
+    auto code = codegen(ctx);
+    code->dump();
     return 0;
 }
