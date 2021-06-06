93a3136cfcff2f4ec0f546742f9bae0a77486b7c
Daming Yang <lion@aosc.io>
2021-06-07 02:24:42 +0800

    codegen: compute add/mul over values
    == Codegen ==
    i64 4213

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 #include <llvm/IR/Value.h>
 #include <llvm/IR/Constants.h>
 #include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/IRBuilder.h>
 
 llvm::Value *codegen(llvm::LLVMContext &ctx) {
-    auto t_int64 = llvm::IntegerType::get(ctx, 64);
-    return llvm::ConstantInt::get(t_int64, 123);
+    llvm::IRBuilder<> builder(ctx);
+    auto v1 = llvm::ConstantInt::get(llvm::IntegerType::get(ctx, 64), 42);
+    auto v2 = llvm::ConstantInt::get(llvm::IntegerType::get(ctx, 64), 100);
+    auto v3 = llvm::ConstantInt::get(llvm::IntegerType::get(ctx, 64), 13);
+    auto v4 = builder.CreateMul(v1, v2);
+    return builder.CreateAdd(v4, v3);
 }
 
 int main() {
